61ddba2dec26655e59c2e3118d6f259d3201da82
erawn <erawn65@gmail.com>
2022-04-07 17:36:44 -0700

    update www editor for testing

--- apps/www/components/Editor.tsx
+++ apps/www/components/Editor.tsx
@@ -20,6 +20,69 @@ const Editor: FC<EditorProps & Partial<TldrawProps>> = ({
 }) => {
   const handleMount = React.useCallback((app: TldrawApp) => {
     window.app = app
+
+
+    app.createShapes( 
+      {
+        id: 'rect1',
+        type: TDShapeType.Rectangle,
+        name: 'Rectangle',
+        childIndex: 1,
+        point: [0, 0],
+        size: [100, 100],
+      },
+      {
+        id: 'rect2',
+        name: 'Rectangle',
+        type: TDShapeType.Rectangle,
+        point: [200, 200],
+        size: [100, 100],
+      }
+    )
+  }, [])
+
+  React.useEffect(() => {
+    let i = 0
+    const interval = setInterval(() => {
+      const app = window.app
+      const rect1 = app.getShape('rect1')
+
+      fetch('http://127.0.0.1:8080/currentVersion').then( response => 
+
+        response.json()
+      ).then( data =>
+        console.log(data)
+        );
+
+
+
+
+
+      if (!rect1) {
+        app.createShapes({
+          id: 'rect1',
+          type: TDShapeType.Rectangle, 
+          name: 'Rectangle',
+          childIndex: 1,
+          point: [0, 0],
+          size: [100, 100],
+        })
+        return
+      }
+
+      const color = i % 2 ? ColorStyle.Green : ColorStyle.Red
+
+      app.updateShapes({
+        id: 'rect1', 
+        style: {
+          ...rect1.style, 
+          color,
+        },
+      })
+
+      i++
+    }, 1000)
+    return () => clearInterval(interval)
   }, [])
 
   // Send events to gtag as actions.
