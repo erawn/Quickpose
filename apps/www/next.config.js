7594fe6ebcba71defa30a7874bb18029f084ba1f
Eric Rawn <erawn@berkeley.edu>
2022-07-17 15:18:24 -0700

    web build

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,7 +1,6 @@
 const withPWA = require('next-pwa')
 const withTM = require('next-transpile-modules')
 const SentryWebpackPlugin = require('@sentry/webpack-plugin')
-const path = require('path');
 const {
   GITHUB_ID,
   GITHUB_API_SECRET,
