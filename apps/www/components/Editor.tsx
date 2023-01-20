77a5ae791a657dcf77f144af45a3f4aca0f4a2d7
Eric Rawn <erawn@berkeley.edu>
2023-01-19 22:44:55 -0800

    gtag types

--- apps/www/components/Editor.tsx
+++ apps/www/components/Editor.tsx
@@ -116,7 +116,7 @@ const Editor = ({ id = 'home', ...rest }: EditorProps & Partial<TldrawProps>) =>
 
         console.log('send fork', id)
         lock
-          .acquire('select', async function () {
+          .acquire('select', async function() {
             await axios
               .get(LOCALHOST_BASE + '/fork/' + id, {
                 timeout: 600,
@@ -124,7 +124,7 @@ const Editor = ({ id = 'home', ...rest }: EditorProps & Partial<TldrawProps>) =>
                   Autorun: app.settings.sketchAutorun,
                 },
               })
-              .then((response) => {
+              .then(response => {
                 if (response.status === 200) {
                   netData.current = response.data
                   dataInterval(netData, graphData, simulation)
@@ -132,15 +132,15 @@ const Editor = ({ id = 'home', ...rest }: EditorProps & Partial<TldrawProps>) =>
                   app.setIsLoading(false)
                 }
               })
-              .catch((error) => {
+              .catch(error => {
                 console.warn('error forking current version: ', error)
                 return null
               })
           })
-          .then(function () {
+          .then(function() {
             app.setIsLoading(false)
           })
-          .catch((error) => {
+          .catch(error => {
             console.warn('error forking current version: ', error)
             return null
           })
@@ -155,7 +155,7 @@ const Editor = ({ id = 'home', ...rest }: EditorProps & Partial<TldrawProps>) =>
       app.setIsLoading(true)
       console.log('send select', id)
       lock
-        .acquire('select', async function () {
+        .acquire('select', async function() {
           await axios
             .get(LOCALHOST_BASE + '/select/' + id, {
               timeout: 600,
@@ -163,7 +163,7 @@ const Editor = ({ id = 'home', ...rest }: EditorProps & Partial<TldrawProps>) =>
                 Autorun: app.settings.sketchAutorun,
               },
             })
-            .then(function (response) {
+            .then(function(response) {
               if (response.status === 200) {
                 //updateThumbnail(app, 'node' + currentVersion.current, currentVersion)
                 currentVersion.current = parseInt(response.data)
@@ -172,12 +172,12 @@ const Editor = ({ id = 'home', ...rest }: EditorProps & Partial<TldrawProps>) =>
                 //drawInterval()
               }
             })
-            .catch((error) => {
+            .catch(error => {
               console.warn('error selecting current version: ', error)
               return null
             })
         })
-        .then(function () {
+        .then(function() {
           app.setIsLoading(false)
         })
     }
@@ -544,6 +544,7 @@ const Editor = ({ id = 'home', ...rest }: EditorProps & Partial<TldrawProps>) =>
           app.document.name,
           true
         )
+        window.gtag('event', 'backup')
         timeSinceLastSave.current = new Date().getTime()
       }
     }
